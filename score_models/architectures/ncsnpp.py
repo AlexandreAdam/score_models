Fixed the log prob ODE integrator, added Heun method and fixed the SDE priors which had the wrong dimensions setup

score_models/architectures/ncsnpp.py
@@ -57,6 +57,9 @@ class NCSNpp(nn.Module):
             condition_num_embedding:tuple[int,...]=None,
             condition_input_channels:int=None,
             condition_vector_channels:int=None,
+            fourier_features=False,
+            n_min=7,
+            n_max=8,
             **kwargs
           ):
         super().__init__()
@@ -205,7 +208,7 @@ class NCSNpp(nn.Module):
         input_pyramid_ch = channels + self.condition_input_channels
         modules.append(conv3x3(channels + self.condition_input_channels, nf, dimensions=dimensions))
         hs_c = [nf]
-        in_ch = nf
+        in_ch = nf + fourier_feature_channels
         for i_level in range(num_resolutions):
             # Residual blocks for this resolution
             for i_block in range(num_res_blocks):
@@ -310,10 +313,16 @@ class NCSNpp(nn.Module):
         temb = modules[m_idx](self.act(temb))
         m_idx += 1
         
+        
         if self.conditioned:
             for j, condition in enumerate(args):
                 if self.condition_type[j].lower() == "input":
                     x = torch.cat([x, condition], dim=1)
+        
+        # Add Fourier features
+        # if self.fourier_features:
+            # ffeatures = self.fourier_features(x)
+            # x = torch.concat([x, ffeatures], axis=1)
         
         # Downsampling block
         input_pyramid = None

score_models/base.py
@@ -14,6 +14,7 @@ import time
 import os, glob, re, json
 import numpy as np
 from datetime import datetime
+from contextlib import nullcontext
 
 from .sde import VESDE, VPSDE, TSVESDE, SDE
 from .utils import load_architecture
@@ -129,15 +130,18 @@ class ScoreModelBase(Module, ABC):
         divergence = (vectors * vjp_func(vectors)[0]).flatten(1).sum(dim=1)
         return divergence
     
+    @torch.no_grad()
     def log_likelihood(
             self, 
-            t,
             x,
             *args,
             ode_steps: int, 
             n_cotangent_vectors: int = 1, 
             noise_type="rademacher", 
-            verbose=0
+            verbose=0,
+            method="Euler",
+            t0:int=0,
+            t1:int=1
             ) -> Tensor:
         """
         A basic implementation of Euler discretisation method of the ODE associated 
@@ -159,39 +163,53 @@ class ScoreModelBase(Module, ABC):
         disable = False if verbose else True  
         B, *D = x.shape
         log_p = 0.
-        dt = t / ode_steps
+        t = torch.ones([B]).to(self.device) * t0
+        dt = (t1 - t0) / ode_steps
+        # Small wrappers to make the notation a bit more readable
+        f = lambda t, x: self.ode_drift(t, x, *args)
+        div = lambda t, x: self.divergence(self.ode_drift, t, x, *args, **kwargs)
         for _ in tqdm(range(ode_steps), disable=disable):
-            x = x + self.ode_drift(t, x, *args) * dt.view(-1, *[1]*len(D))
-            log_p += self.divergence(self.ode_drift, t, x, *args, **kwargs) * dt
-            t = t + dt
-        log_p = self.sde.prior(D).log_prob(x)
+            if method == "Euler":
+                x = x + f(t, x) * dt
+                log_p += div(t, x) * dt
+                t = t + dt
+            elif method == "Heun":
+                previous_x = x.clone()
+                drift = f(t, x)
+                new_x = x + drift * dt
+                x = x + 0.5 * (drift + f(t+dt, new_x)) * dt
+                log_p += 0.5 * (div(t, previous_x) + div(t+dt, x)) * dt
+                t = t + dt
+            else:
+                raise NotImplementedError("Invalid method, please select either Euler or Heun")
+        log_p += self.sde.prior(D).log_prob(x)
         return log_p
     
-    def score_at_zero_temperature(
-            self, 
-            x,
-            *args,
-            ode_steps: int, 
-            n_cotangent_vectors: int = 1, 
-            noise_type="rademacher", 
-            verbose=0,
-            return_ll=False
-            ) -> Tensor:
-        """
-        Takes a gradient through the log_likelihood solver to compute the score 
-        at zero temperature. 
-        """
-        kwargs = {"ode_steps": ode_steps, 
-                  "n_cotangent_vectors": n_cotangent_vectors, 
-                  "verbose": verbose,
-                  "noise_type": noise_type
-                  }
-        wrapped_ll = lambda x: self.log_likelihood(x, *args, **kwargs)
-        ll, vjp_func = vjp(wrapped_ll, x)
-        score = vjp_func(torch.ones_like(ll))
-        if return_ll:
-            return score, ll
-        return score
+    # def score_at_zero_temperature(
+            # self, 
+            # x,
+            # *args,
+            # ode_steps: int, 
+            # n_cotangent_vectors: int = 1, 
+            # noise_type="rademacher", 
+            # verbose=0,
+            # return_ll=False
+            # ) -> Tensor:
+        # """
+        # Takes a gradient through the log_likelihood solver to compute the score 
+        # at zero temperature. 
+        # """
+        # kwargs = {"ode_steps": ode_steps, 
+                  # "n_cotangent_vectors": n_cotangent_vectors, 
+                  # "verbose": verbose,
+                  # "noise_type": noise_type
+                  # }
+        # wrapped_ll = lambda x: self.log_likelihood(x, *args, **kwargs)
+        # ll, vjp_func = vjp(wrapped_ll, x)
+        # score = vjp_func(torch.ones_like(ll))
+        # if return_ll:
+            # return score, ll
+        # return score
     
     @torch.no_grad()
     def sample(

score_models/ode/heun.py
@@ -4,9 +4,9 @@ import torch
 from torch import Tensor
 from tqdm import tqdm
 
-def euler(x:Tensor, drift_fn:Callable, N:int, t0=0., t1=1., verbose=0, **kwargs):
+def heun(x:Tensor, drift_fn:Callable, N:int, t0=0., t1=1., verbose=0, **kwargs):
         """
-        A basic implementation of Euler discretisation method of the ODE associated 
+        A basic implementation of Heun second order discretisation method of the ODE associated 
         with the marginales of the learned SDE.
         
         x: Initial state
@@ -22,7 +22,8 @@ def euler(x:Tensor, drift_fn:Callable, N:int, t0=0., t1=1., verbose=0, **kwargs)
         t = torch.ones([B]).to(x.device) * t0
         dt = (t1 - t0) / N
         for _ in tqdm(range(N), disable=disable):
-            x = x + drift_fn(t, x, **kwargs) * dt
-            t += dt
+            new_x = x + drift_fn(t, x) * dt
+            x = x + 0.5 * (drift_fn(t, x) + drift_fn(t+dt, new_x)) * dt
+            t = t + dt
         return x
     

score_models/sde/tsvesde.py
@@ -66,7 +66,7 @@ class TSVESDE(SDE):
     def prior(self, shape):
         mu = torch.zeros(shape)
         sigma_max = np.exp(-self.beta * (1. - self.t_star) + np.log(self.sigma_max))
-        return Independent(Normal(loc=mu, scale=sigma_max, validate_args=False), 1)
+        return Independent(Normal(loc=mu, scale=sigma_max, validate_args=False), len(shape))
     
     def marginal_prob_scalars(self, t) -> tuple[Tensor, Tensor]:
         return self.scale(t), self.sigma(t)

score_models/sde/vesde.py
@@ -40,7 +40,7 @@ class VESDE(SDE):
             mu = torch.zeros(shape)
         else:
             assert mu.shape == shape 
-        return Independent(Normal(loc=mu, scale=self.sigma_max, validate_args=False), 1)
+        return Independent(Normal(loc=mu, scale=self.sigma_max, validate_args=False), len(shape))
     
     def marginal_prob_scalars(self, t) -> tuple[Tensor, Tensor]:
         return torch.ones_like(t), self.sigma(t)

score_models/sde/vpsde.py
@@ -25,7 +25,7 @@ class VPSDE(SDE):
         
     def prior(self, shape):
         mu = torch.zeros(shape)
-        return Independent(Normal(loc=mu, scale=1., validate_args=False), 1)
+        return Independent(Normal(loc=mu, scale=1., validate_args=False), len(shape))
 
     def diffusion(self, t: Tensor, x: Tensor) -> Tensor:
         _, *D = x.shape