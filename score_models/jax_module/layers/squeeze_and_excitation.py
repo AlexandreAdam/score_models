8616f8af41ffe144a0019b1efbb3c799ae616e95
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-02-07 11:49:51 -0500

    Added two more scripts

--- score_models/jax_module/layers/squeeze_and_excitation.py
+++ score_models/jax_module/layers/squeeze_and_excitation.py
@@ -1,25 +1,29 @@
 import jax.numpy as jnp
-from jax import nn as jnn
-from jax import random
-import flax.linen as nn
+import equinox as eqx
+from jax.nn import sigmoid
 
 
-class SqueezeAndExcite(nn.Module):
-    channels: int
-    hidden_units: int
+class SqueezeAndExcite(eqx.Module):
+    """
+    Implementation of the Squeeze and Excite module, a form of channel attention
+    originally described in Hu et al (2019). Squeeze-and-Excitation Networks, https://arxiv.org/abs/1709.01507
+    """
+    excite_network: eqx.Module
 
-    def setup(self):
-        self.excite_network = nn.Sequential(
-            [
-                nn.Dense(self.hidden_units),
-                nn.relu,
-                nn.Dense(self.channels),
-            ]
-        )
+    def __init__(self, channels, hidden_units, *, key):
+        keys = eqx.split_key(key, 3)
+        self.excite_network = eqx.nn.Sequential([
+            eqx.nn.Linear(channels, hidden_units, key=keys[0]),
+            eqx.nn.ReLU(),
+            eqx.nn.Linear(hidden_units, channels, key=keys[1])
+        ])
 
     def __call__(self, x):
         B, C, H, W = x.shape
-        z = jnp.mean(x, axis=(2, 3))  # Squeeze operation is a global average
-        z = self.excite_network(z)  # compute channel importance
-        s = jnn.sigmoid(z).reshape((B, C, 1, 1))
-        return s * x  # scale channels
+        # Squeeze operation is a global average
+        z = jnp.mean(x, axis=(2, 3))
+        # Compute channel importance
+        z = self.excite_network(z)
+        s = sigmoid(z).reshape((B, C, 1, 1))
+        return s * x  # Scale channels
+
