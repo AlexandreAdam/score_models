6cda3d3aa5c93b6aef811183410fbd028c540d66
Connor Stone <connorstone628@gmail.com>
2024-05-28 09:20:50 -0400

    merge conv likelihood and diag version

--- score_models/simple_models/conv_likelihood.py
+++ score_models/simple_models/conv_likelihood.py
@@ -5,11 +5,9 @@ from torch.func import grad
 
 
 class ConvolvedLikelihood(nn.Module):
-    def __init__(self, sde, y, sigma_y, A=None, f=None, AAT=None):
-        assert (A is not None) + (f is not None) == 1, "Either A or f must be provided (not both)"
-        assert (A is not None) + (
-            AAT is not None
-        ) == 1, "Either A or AAT must be provided (not both)"
+    def __init__(self, sde, y, sigma_y, A=None, f=None, AAT=None, diag=False):
+        assert (A is not None) != (f is not None), "Either A or f must be provided (not both)"
+        assert (A is not None) | (AAT is not None), "Either A or AAT must be provided"
         super().__init__()
         self.sde = sde
         self.y = y
@@ -20,26 +18,27 @@ class ConvolvedLikelihood(nn.Module):
             self.AAT = A @ A.T
         else:
             self.AAT = AAT
+        assert self.sigma_y.shape == self.AAT.shape, "sigma_y and AAT must have the same shape"
+        self.diag = diag
         self.hyperparameters = {"nn_is_energy": True}
 
-    def forward(self, t, xt, **kwargs):
+    @property
+    def diag(self):
+        return self._diag
+
+    @diag.setter
+    def diag(self, value):
+        self._diag = value
+        self.forward = self.diag_forward if value else self.full_forward
+
+    def diag_forward(self, t, xt, **kwargs):
+        r = self.y - self.f(xt.squeeze())
+        sigma = 1 / (self.sigma_y + self.sde.sigma(t) ** 2 * self.AAT)
+        ll = 0.5 * torch.sum(r**2 * sigma)
+        return ll.unsqueeze(0) * self.sde.sigma(t)
+
+    def full_forward(self, t, xt, **kwargs):
         r = self.y - self.A @ xt.squeeze()
         sigma = torch.inverse(self.sigma_y + self.sde.sigma(t) ** 2 * self.AAT)
         ll = 0.5 * (r @ sigma @ r.reshape(1, r.shape[0]).T)
         return ll.unsqueeze(0) * self.sde.sigma(t)
-
-
-class ConvolvedLikelihood_diag(nn.Module):
-    def __init__(self, sde, y, sigma_y, AAT, f):
-        super().__init__()
-        self.sde = sde
-        self.y = y
-        self.sigma_y = sigma_y
-        self.AAT = AAT
-        self.f = f
-        self.hyperparameters = {"nn_is_energy": True}
-
-    def forward(self, t, xt, **kwargs):
-        r = self.y - self.f(xt.squeeze())
-        sigma = 1 / (self.sigma_y + self.sde.sigma(t) ** 2 * self.AAT)
-        return 0.5 * torch.sum(r**2 * sigma).unsqueeze(0) * self.sde.sigma(t)
