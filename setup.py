e0a6819b91d643f70b224dd277c9a82484268dd8
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-10-07 15:08:52 -0400

    Updated actions

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-# Read the contents of the README file
-with open("long_description.rst", "r") as fh:
-    long_description = fh.read()
-
-setup(
-	name="score_models",
-	version="0.6.0",
-    description="A simple pytorch interface for score model and basic diffusion.",
-    long_description=long_description,
-    author="Alexandre Adam",
-    author_email="alexandre.adam@umontreal.ca",
-    url="https://github.com/AlexandreAdam/torch_score_models",
-    packages=find_packages(),
-    install_requires=[
-        "torch>=2.0",
-        "scipy",
-        "torch_ema",
-        "h5py",
-        "numpy",
-        "tqdm",
-        "peft>=0.11"
-    ],
-	python_requires=">=3.9",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
-    ],
-)
-
+if __name__ == "__main__":
+    setuptools.setup()
