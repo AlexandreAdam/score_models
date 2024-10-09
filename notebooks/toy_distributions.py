860a8bbacc594423ffa2409090384f4bdd8bdbb5
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-10-08 21:59:16 -0400

    Modified docs action and added utils for plotting and distributions

---
+++ notebooks/toy_distributions.py
@@ -0,0 +1,104 @@
+import torch
+import numpy as np
+from torch.distributions import constraints
+from torch import distributions as tfd
+
+def swiss_roll(modes=128, size=0.1, width=0.1, spread=0.7, device=DEVICE) -> tfd.Distribution:
+    """
+    Returns a swiss roll distribution from of a mixture of <modes> gaussian distributions
+        :param modes: Number of modes in the mixture
+        :param size: Size of the swiss roll
+        :param width: Width of the modes in the swiss roll
+        :param spread: Angular spread of the modes in the swiss roll
+    """
+    assert (spread > 0)
+    t = 1.5 * np.pi * (1 + 2 * torch.linspace(0, 1, modes)**(spread))
+    x = t * torch.cos(t)
+    y = t * torch.sin(t)
+    coords = size * torch.stack([x, y], dim=1).to(device)
+    mixture = tfd.Categorical(probs=torch.ones(modes).to(device), validate_args=False)
+    component = tfd.Independent(tfd.Normal(loc=coords, scale=width, validate_args=False), 1)
+    return tfd.MixtureSameFamily(mixture, component, validate_args=False)
+
+
+def five_swiss_rolls(modes=128, size=0.1, width=0.1, spread=0.7, offset=3, device=DEVICE):
+    """
+    Returns 5 swiss roll distribution arranged in the classic 5 dots pattern
+        :param modes: Number of modes in the mixture
+        :param size: Size of the swiss roll
+        :param width: Width of the modes in the swiss roll
+        :param spread: Angular spread of the modes in the swiss roll
+        :param offset: Distance between the swiss rolls
+    """
+    assert (spread > 0)
+    offsets = [[-offset, offset], [offset, -offset], [offset, offset], [-offset, -offset]]
+    coords = []
+    for k in range(5):
+        t = 1.5 * np.pi * (1 + 2 * torch.linspace(0, 1, modes)**(spread))
+        x = size * t * torch.cos(t)
+        y = size * t * torch.sin(t)
+        if k > 0:
+            x += offsets[k-1][0]
+            y += offsets[k-1][1]
+        coords.append(torch.stack([x, y], dim=1))
+    coords = torch.concat(coords, dim=0).to(device)
+    mixture = tfd.Categorical(probs=torch.ones(5*modes).to(device), validate_args=False)
+    component = tfd.Independent(tfd.Normal(loc=coords, scale=width, validate_args=False), 1)
+    return tfd.MixtureSameFamily(mixture, component, validate_args=False)
+
+
+def two_moons(modes=128, width=0.1, size=1, device=DEVICE) -> tfd.Distribution:
+    """
+    Returns a 2 moons distributions from a mixture of <modes> gaussian distributions
+    :param modes: Number of modes inside each moon
+    :param width: Width of the moons
+    :param size: scales the coordinates by this amount
+    """
+    outer_circ_x = torch.cos(torch.linspace(0, np.pi, modes)) - .5
+    outer_circ_y = torch.sin(torch.linspace(0, np.pi, modes)) - .25
+    inner_circ_x = - torch.cos(torch.linspace(0, np.pi, modes)) + .5
+    inner_circ_y = - torch.sin(torch.linspace(0, np.pi, modes)) + .25
+    x = torch.concat([outer_circ_x, inner_circ_x])
+    y = torch.concat([outer_circ_y, inner_circ_y])
+    coords = size * torch.stack([x, y], dim=1).to(device)
+    mixture = tfd.Categorical(probs=torch.ones(2*modes).to(device), validate_args=False)  # Uniform
+    component = tfd.Independent(tfd.Normal(loc=coords, scale=width, validate_args=False), 1)  # Diagonal Multivariate Normal
+    return tfd.MixtureSameFamily(mixture, component, validate_args=False)
+
+
+def checkerboard(squares: int = 4, size: float = None) -> tfd.Distribution:
+    """
+    Checkerboard distribution
+        :param squares: Number of squares on a side
+        :param size: Size of the checkerboard. By default, size = squares.
+    """
+    if size is None:
+        size = squares
+    square_size = float(size / squares)
+    x = torch.arange(-squares // 2 + squares % 2, squares // 2, 1)
+    xx, yy = torch.meshgrid(x, x)
+    mask = (((xx % 2) & (yy % 2)) + (((xx + 1) % 2) & ((yy + 1) % 2))).bool()
+    coords = torch.stack([xx[mask].ravel(), yy[mask].ravel()], dim=1).float().to(DEVICE)
+    coords *= square_size
+    mixture = tfd.Categorical(probs=torch.ones(squares**2 // 2), validate_args=False)
+    component = tfd.Independent(tfd.Uniform(low=coords, high=coords + square_size, validate_args=False), 1)
+    return tfd.MixtureSameFamily(mixture, component, validate_args=False)
+
+
+def egg_box(modes: int = 16, mode_width: float = 0.5, box_size: float = None, device=DEVICE) -> tfd.Distribution:
+    """
+    Returns gaussian mixture equally spaces on the 2d plane.
+        :param modes: Number of modes in the mixture
+        :param box_size: Size egg box
+        :param mode_width: Width of the modes
+    """
+    assert int(np.sqrt(modes))**2 == modes, f"modes = {modes} is not a square number"
+    if box_size is None:
+        box_size = modes
+    x = torch.linspace(-1, 1, int(np.sqrt(modes))).to(device)
+    x, y = torch.meshgrid(x, x, indexing="xy")
+    coords = box_size * torch.stack([x.ravel(), y.ravel()], dim=1)
+    mixture = tfd.Categorical(probs=torch.ones(modes).to(device), validate_args=False)
+    component = tfd.Independent(tfd.Normal(loc=coords, scale=mode_width, validate_args=False), 1)
+    return tfd.MixtureSameFamily(mixture, component, validate_args=False)
+
---
+++ notebooks/utils.py
@@ -0,0 +1,182 @@
+from typing import Callable
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import matplotlib.cm as cm
+import scienceplots # pip install SciencePlots
+import colorcet as cc # pip install colorcet
+import pylab
+import torch
+from scipy.interpolate import interpn
+from scipy.special import logsumexp
+from matplotlib.colors import Normalize
+
+plt.style.use('dark_background')
+plt.style.use('science')
+params = {
+         'axes.labelsize': 18,
+         'axes.titlesize': 30,
+         'ytick.labelsize' :20,
+         'xtick.labelsize' :20,
+         'legend.fontsize': 16,
+         'xtick.major.size': 8,
+         'xtick.minor.size': 4,
+         'ytick.major.size': 8,
+         'ytick.minor.size': 4,
+         }
+pylab.rcParams.update(params)
+
+
+def plot_density(
+        logp_fn: Callable,
+        fig=None, 
+        ax=None, 
+        extent=(-2, 2, -2, 2),
+        dx=0.01, 
+        dy=0.01, 
+        colorbar=False, 
+        cmap=cc.cm.fire, 
+        vmin=None, 
+        vmax=None, 
+        **kwargs):
+    """
+    Plot the density of a log probability function
+    """
+    # Generate a grid of points
+    (xmin, xmax, ymin, ymax) = extent
+    x = np.arange(xmin, xmax, dx)
+    y = np.arange(ymin, ymax, dy)
+    n = x.size
+    m = y.size
+    points = np.stack(np.meshgrid(x, y, indexing="xy"), axis=-1).reshape((-1, 2)) 
+    # Compute log probability and normalize to get the density
+    logp = logp_fn(torch.tensor(points).to(DEVICE)).detach().numpy().reshape([m, n])
+    p = np.exp(logp - logsumexp(logp))
+    # Plot the density
+    norm = Normalize(vmin=p.min() if vmin is None else vmin, vmax=p.max() if vmax is None else vmax)
+    if ax is None:
+        fig, ax = plt.subplots()
+    im = ax.imshow(p, extent=extent, cmap=cmap, norm=norm, **kwargs)
+    if colorbar:
+        cax = fig.colorbar(im, ax=ax, fraction=0.038, pad=0.02)
+        cax.set_ylabel(r'$p(\mathbf{x})$')
+    return ax
+
+def plot_scatter(
+        points, 
+        fig=None, 
+        ax=None, 
+        bins=20, 
+        sort=True, 
+        norm=None, 
+        ticks=None, 
+        colorbar=False, 
+        cmap=cc.cm.fire, 
+        **kwargs):
+    """
+    Scatter plot colored by 2d histogram
+    """
+    x = points[:, 0]
+    y = points[:, 1]
+    # Compute histogram and intepolate density
+    data, x_edges, y_edges = np.histogram2d(x, y, bins=bins, density=True)
+    x_bins = 0.5 * (x_edges[1:] + x_edges[:-1])
+    y_bins = 0.5 * (y_edges[1:] + y_edges[:-1])
+    z = interpn((x_bins, y_bins), data, np.vstack([x, y]).T, method="splinef2d", bounds_error=False)
+    z[np.where(np.isnan(z))] = 0.0
+    # Sort the points by density, so that the densest points are plotted last
+    if sort:
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+    if ax is None:
+        fig, ax = plt.subplots()
+    if norm is None:
+        norm = Normalize(vmin=z.min(), vmax=z.max())
+    ax.scatter(x, y, c=z, cmap=cmap, norm=norm, **kwargs)
+    if fig is not None and colorbar:
+        sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+        sm.set_array([])
+        box = ax.get_position()
+        cax = plt.axes([box.x0*1.01 + box.width * 1.05, box.y0, 0.02, box.height])
+        fig.colorbar(sm, cax=cax, ticks=ticks)
+        cax.set_ylabel(r'$p(\mathbf{x})$')
+    return ax
+
+
+def plot_score_field(
+        score_fn: Callable,
+        fig=None,
+        ax=None,
+        extent=(-2, 2, -2, 2),
+        n=20, 
+        dx=0.05, 
+        scale=1.5e3, 
+        width=0.007,
+        colorbar=False,
+        cmap=cc.cm.fire,
+        ):
+    """
+    Plot a vector field
+    """
+    # Generate a grid of points
+    (xmin, xmax, ymin, ymax) = extent
+    x = np.linspace(xmin, xmax, n)
+    y = np.linspace(ymin, ymax, n)
+    points = np.stack(np.meshgrid(x, y), axis=-1).reshape((-1, 2))
+    g = score_fn(torch.tensor(points).to(DEVICE).float()).detach().numpy().reshape([n, n, 2])
+    colors = np.sqrt(g[..., 0]**2 + g[..., 1]**2).ravel()
+    if ax is None:
+        fig, ax = plt.subplots()
+    norm = Normalize()
+    ax.quiver(x, y, g[..., 0], g[..., 1], color=cmap(norm(colors)), scale=scale, width=width)
+    if fig is not None:
+        sm = cm.ScalarMappable(norm=norm, cmap=cmap)
+        sm.set_array([])
+        box = ax.get_position()
+        cax = plt.axes([box.x0*1.01 + box.width * 1.05, box.y0, 0.02, box.height])
+        fig.colorbar(sm, cax=cax)
+        cax.set_ylabel(r"$\lVert \nabla \log p(\mathbf{x}) \rVert$")
+    return ax
+
+def plot_density_contours(
+        logp_fn: Callable,
+        fig=None,
+        ax=None,
+        extent=(-2, 2, -2, 2),
+        ci: tuple = (0.68, 0.95, 0.99),
+        dx=0.01, 
+        dy=0.01,
+        cmap=cc.cm.fire,
+        ):
+    """
+    Plot density contours
+    """
+    (xmin, xmax, ymin, ymax) = extent
+    x = np.arange(xmin, xmax, dx)
+    y = np.arange(ymin, ymax, dy)
+    n = x.size
+    m = y.size
+    points = np.stack(np.meshgrid(x, y), axis=-1).reshape((-1, 2))
+    # Compute log probability and normalize to get the density
+    logp = logp_fn(torch.tensor(points).to(DEVICE)).detach().numpy().reshape([m, n])
+    p = np.exp(logp - logsumexp(logp))
+    # Compute the cumulative probability
+    cumul = np.sort(p.ravel() * dx * dy)[::-1].cumsum()
+    ps = []
+    for _ci in ci:
+        p_at_ci = np.sort(p.ravel())[::-1][np.argmin((cumul - _ci)**2)]
+        ps.append(p_at_ci)
+    colors = [cmap(i/(len(ci)-1)) for i in range(len(ci))]
+    contours = ax.contour(x, y, p, levels=ps[::-1], colors=colors, linewidths=2, linestyles="--")
+    if ax is None:
+        fig, ax = plt.subplots()
+    def fmt(x):
+        ci = cumul[np.argmin((x - np.sort(p.ravel())[::-1])**2)]
+        s = f"{ci*100:.1f}"
+        if s.endswith("0"):
+            s = f"{ci*100:.0f}"
+        return rf"{s} \%" if plt.rcParams["text.usetex"] else f"{s} %"
+    ax.clabel(contours, contours.levels, inline=True, fmt=fmt, fontsize=10)
+    return ax
+
