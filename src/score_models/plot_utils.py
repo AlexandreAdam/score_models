Fixed issue with Hessian Diagonal model log likelihood not working properly with the sampler. Now all the logic is built in properly. Also refactored log_prob to log_likelihood

src/score_models/plot_utils.py
@@ -38,8 +38,8 @@ def plot_density(
         fig=None, 
         ax=None, 
         extent=(-2, 2, -2, 2),
-        dx=0.01, 
-        dy=0.01, 
+        dx=0.025, 
+        dy=0.025,
         colorbar=False, 
         cmap=cmap, 
         vmin=None, 
@@ -54,10 +54,10 @@ def plot_density(
     y = np.arange(ymin, ymax, dy)
     n = x.size
     m = y.size
-    points = np.stack(np.meshgrid(x, y, indexing="xy"), axis=-1).reshape((-1, 2)) 
+    points = np.stack(np.meshgrid(x, y, indexing="xy"), axis=-1).reshape((-1, 2))
     # Compute log probability and normalize to get the density
-    logp = logp_fn(torch.tensor(points).to(DEVICE)).detach().numpy().reshape([m, n])
-    p = np.exp(logp - logsumexp(logp))
+    logp = logp_fn(torch.tensor(points).to(DEVICE).float()).detach().numpy().reshape([m, n])
+    p = np.exp(logp - logsumexp(logp + np.log(dx) + np.log(dy)))
     # Plot the density
     norm = Normalize(vmin=p.min() if vmin is None else vmin, vmax=p.max() if vmax is None else vmax)
     if ax is None:
@@ -149,8 +149,8 @@ def plot_contours(
         ax=None,
         extent=(-2, 2, -2, 2),
         ci: tuple = (0.68, 0.95, 0.99),
-        dx=0.01, 
-        dy=0.01,
+        dx=0.025, 
+        dy=0.025,
         cmap=cc.cm.fire,
         ):
     """

tests/test_score_models.py
@@ -19,19 +19,19 @@ def local_test_loading_model_and_score_fn():
 
 
 def test_loading_from_string():
-    score = ScoreModel("mlp", sigma_min=1e-2, sigma_max=10, dimensions=2)
+    score = ScoreModel("mlp", sigma_min=1e-2, sigma_max=10, channels=2)
     print(score.sde)
     x = torch.randn(1, 2)
     t = torch.ones(1)
     score(t, x)
 
-    score = EnergyModel("mlp", sigma_min=1e-2, sigma_max=10, dimensions=2)
+    score = EnergyModel("mlp", sigma_min=1e-2, sigma_max=10, channels=2)
     print(score.sde)
     x = torch.randn(1, 2)
     t = torch.ones(1)
     score(t, x)
 
-    score = EnergyModel("mlp", sigma_min=1e-2, sigma_max=10, dimensions=2, nn_is_energy=True)
+    score = EnergyModel("mlp", sigma_min=1e-2, sigma_max=10, channels =2, nn_is_energy=True)
     print(score.sde)
     x = torch.randn(1, 2)
     t = torch.ones(1)
@@ -45,21 +45,21 @@ def test_loading_from_string():
 
 
 def test_loading_with_nn():
-    net = MLP(dimensions=2)
+    net = MLP(2)
     score = ScoreModel(net, sigma_min=1e-2, sigma_max=10)
     print(score.sde)
     x = torch.randn(1, 2)
     t = torch.ones(1)
     score(t, x)
 
-    net = MLP(dimensions=2)
+    net = MLP(2)
     score = EnergyModel(net, sigma_min=1e-2, sigma_max=10)
     print(score.sde)
     x = torch.randn(1, 2)
     t = torch.ones(1)
     score(t, x)
 
-    net = MLP(dimensions=2, nn_is_energy=True)
+    net = MLP(2, nn_is_energy=True)
     score = EnergyModel(net, sigma_min=1e-2, sigma_max=10)
     print(score.sde)
     x = torch.randn(1, 2)
@@ -80,16 +80,16 @@ def test_init_score():
         score = ScoreModel(net)
 
 
-def test_log_likelihood():
-    net = MLP(dimensions=2)
+def test_log_prob():
+    net = MLP(channels=2)
     score = ScoreModel(net, beta_min=1e-2, beta_max=10)
     print(score.sde)
     x = torch.randn(3, 2)
-    ll = score.log_likelihood(x, steps=10, verbose=1, method="euler_ode")
+    ll = score.log_prob(x, steps=10, verbose=1, method="euler_ode")
     print(ll)
     assert ll.shape == torch.Size([3])
 
-    ll = score.log_likelihood(x, steps=10, verbose=1, method="rk2_ode")
+    ll = score.log_prob(x, steps=10, verbose=1, method="rk2_ode")
     print(ll)
     assert ll.shape == torch.Size([3])
 

tests/test_solvers.py
@@ -87,7 +87,7 @@ def test_solver_forward(solver, mean, cov):
     slvr = Solver(model, solver=solver)
 
     x0 = torch.tensor(np.random.multivariate_normal(mean, cov, 100), dtype=torch.float32)
-    xT = slvr(x0, steps=50, forward=True, get_delta_logp="ode" in solver, progress_bar=False)
+    xT = slvr(x0, steps=50, forward=True, return_logp="ode" in solver, progress_bar=False)
 
     if "ode" in solver:  # check delta_logp calculation for ODE solvers
         xT, dlogp = xT