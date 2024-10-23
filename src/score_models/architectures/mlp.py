Fixed issue with Hessian Diagonal model log likelihood not working pr… (#12)
* Fixed issue with Hessian Diagonal model log likelihood not working properly with the sampler. Now all the logic is built in properly. Also refactored log_prob to log_likelihood

* Changed return_logp to return_dlogp, removed unused arguments in docstrings

* Added docstring for dlogp in ODE Solver class

src/score_models/architectures/mlp.py
@@ -18,14 +18,11 @@ __all__ = ["MLP"]
 class MLP(nn.Module):
     def __init__(
         self,
-        channels: Optional[int] = None,
-        units: int = 100,
-        layers: int = 2,
-        time_branch_channels: int = 32,
-        time_branch_layers: int = 1,
+        channels: int,
+        width: int = 100,
+        layers: int = 4,
         fourier_scale: int = 16,
-        activation: int = "swish",
-        bottleneck: Optional[int] = None,
+        activation: int = "silu",
         attention: bool = False,
         nn_is_energy: bool = False,
         output_activation: str = None,
@@ -39,13 +36,10 @@ class MLP(nn.Module):
 
         Parameters:
         - channels (Optional[int]): Number of input channels. Default is None.
-        - units (int): Number of units in each hidden layer. Default is 100.
+        - width (int): Number of units in each hidden layer. Default is 100.
         - layers (int): Number of hidden layers. Default is 2.
-        - time_branch_channels (int): Number of channels in the time branch. Default is 32.
-        - time_branch_layers (int): Number of layers in the time branch. Default is 1.
         - fourier_scale (int): Scale factor for Fourier features. Default is 16.
         - activation (str): Activation function to use. Default is "swish".
-        - bottleneck (Optional[int]): Number of units in the bottleneck layer. Default is None.
         - attention (bool): Whether to use attention mechanism. Default is False.
         - nn_is_energy (bool): Whether the neural network represents energy. Default is False.
         - output_activation (str): Activation function for the output layer. Default is None.
@@ -61,27 +55,12 @@ class MLP(nn.Module):
         self.condition_type = conditions
         self.condition_embeddings = condition_embeddings
         self.condition_channels = condition_channels
-        # Some backward compatibility
-        if "embedding_scale" in kwargs:
-            fourier_scale = kwargs["embedding_scale"]
-        if "time_embedding_dimensions" in kwargs:
-            time_branch_channels = kwargs["time_embedding_dimensions"]
-        if channels is None:
-            if "dimensions" in kwargs:
-                channels = kwargs["dimensions"]
-            else:
-                raise ValueError(
-                    "You must provide a 'channels' argument to initialize the MLP architecture."
-                )
         self.hyperparameters = {
             "channels": channels,
-            "units": units,
+            "width": width,
             "layers": layers,
-            "time_branch_channels": time_branch_channels,
             "fourier_scale": fourier_scale,
             "activation": activation,
-            "time_branch_layers": time_branch_layers,
-            "botleneck": bottleneck,
             "attention": attention,
             "nn_is_energy": nn_is_energy,
             "output_activation": output_activation,
@@ -89,99 +68,74 @@ class MLP(nn.Module):
             "condition_channels": condition_channels,
             "condition_embeddings": condition_embeddings,
         }
-        self.time_branch_layers = time_branch_layers
         self.layers = layers
+        self.attention = attention
         self.nn_is_energy = nn_is_energy
         if layers % 2 == 1:
-            print(
-                f"Number of layers must be an even number for this architecture. Adding one more layer..."
-            )
             layers += 1
 
         ########### Conditional branch ###########
         if self.conditioned:
             total_time_channels, total_input_channels = conditional_branch(
                 self,
-                time_branch_channels=time_branch_channels,
+                time_branch_channels=width,
                 input_branch_channels=channels,
                 condition_embeddings=condition_embeddings,
                 condition_channels=condition_channels,
                 fourier_scale=fourier_scale,
-            )  # This method attach a Module list to self.conditional_branch
+            )
         else:
-            total_time_channels = time_branch_channels
+            total_time_channels = width
             total_input_channels = channels
-        #########################################
 
         ########### Time branch ###########
-        t_dim = time_branch_channels
-        modules = [
-            GaussianFourierProjection(t_dim, scale=fourier_scale),  # Time embedding
-            nn.Linear(
-                total_time_channels, t_dim
-            ),  # Compress the signal from time index and the other conditionals if any
+        time_branch = [
+            GaussianFourierProjection(width, scale=fourier_scale),
+            nn.Linear(total_time_channels, width),
+            nn.Linear(width, width),
         ]
-        for _ in range(time_branch_layers - 1):
-            modules.append(nn.Linear(t_dim, t_dim))
-        ###################################
+        self.time_branch = nn.ModuleList(time_branch)
 
         ########### Input branch ###########
-        modules.append(nn.Linear(total_input_channels + t_dim, units))
-        if bottleneck is not None:
-            assert isinstance(bottleneck, int)
-            self.bottleneck = bottleneck
-            self.bottleneck_in = nn.Linear(units, bottleneck)
-            self.bottleneck_out = nn.Linear(bottleneck, units)
-        else:
-            self.bottleneck = 0
-        self.attention = attention
-        if self.attention:
-            self.temb_to_bottleneck = nn.Linear(t_dim, bottleneck)
-            self.attention_layer = ScaledAttentionLayer(bottleneck)
+        modules = []
+        self.input_layer = nn.Linear(total_input_channels, width)
         for _ in range(layers):
-            modules.append(nn.Linear(units, units))
+            modules.append(nn.Linear(2*width, width))
+        self.linear_layers = nn.ModuleList(modules)
+        if self.attention:
+            self.attention_layer = ScaledAttentionLayer(width)
         if nn_is_energy:
-            self.output_layer = nn.Linear(units, 1)
+            self.output_layer = nn.Linear(2*width, 1)
             self.output_act = get_activation(output_activation)
         else:
-            self.output_layer = nn.Linear(units, channels)
+            self.output_layer = nn.Linear(2*width, channels)
         self.act = get_activation(activation)
-        self.all_modules = nn.ModuleList(modules)
-        ###################################
 
     def forward(self, t, x, *args, **kwargs):
         B, D = x.shape
-        modules = self.all_modules
 
-        # Time branch
-        temb = modules[0](t)
+        ########### Time branch ###########
+        temb = self.time_branch[0](t)
         if self.conditioned:
             temb = merge_conditional_time_branch(self, temb, *args)
-        i = 1
-        for _ in range(self.time_branch_layers):
-            temb = self.act(modules[i](temb))
-            i += 1
+        for layer in self.time_branch[1:]:
+            temb = self.act(layer(temb))
 
-        # Input branch
-        x = torch.cat([x, temb], dim=1)
+        ########### Input branch ###########
         if self.conditioned:
             x = merge_conditional_input_branch(self, x, *args)
-        x = modules[i](x)
-        i += 1
-        for _ in range(self.layers // 2):
-            x = self.act(modules[i](x))
-            i += 1
-        if self.bottleneck:
-            x = self.act(self.bottleneck_in(x))
+        x = self.input_layer(x)
+        for layer in self.linear_layers[:self.layers//2]:
+            x = torch.cat([x, temb], dim=1)
+            x = self.act(layer(x))
         if self.attention:
-            temb = self.temb_to_bottleneck(temb)
-            context = torch.stack([x, temb], dim=1)
-            x = self.attention_layer(x.view(B, 1, -1), context).view(B, -1)
-        if self.bottleneck:
-            x = self.act(self.bottleneck_out(x))
-        for _ in range(self.layers // 2):
-            x = self.act(modules[i](x))
-            i += 1
+            context = temb.view(B, 1, -1)
+            query = x.view(B, 1, -1)
+            x = self.attention_layer(query, context).view(B, -1)
+        for layer in self.linear_layers[self.layers//2:]:
+            x = torch.cat([x, temb], dim=1)
+            x = self.act(layer(x))
+        x = torch.cat([x, temb], dim=1)
         out = self.output_layer(x)
         if self.nn_is_energy:
             out = self.output_act(out)

src/score_models/sbm/hessian_model.py
@@ -1,6 +1,7 @@
 from typing import Optional, Literal
 
 from torch import Tensor
+from torch.func import vjp
 import torch
 import os
 
@@ -8,9 +9,14 @@ from .base import Base
 from .score_model import ScoreModel
 from ..sde import SDE
 from ..utils import DEVICE
-from ..losses import second_order_dsm, second_order_dsm_meng_variation
+from ..losses import (
+        second_order_dsm, 
+        second_order_dsm_meng_variation,
+        )
+from ..solver import ODESolver
 
 __all__ = ["HessianDiagonal"]
+
 
 class HessianDiagonal(Base):
     def __init__(
@@ -21,7 +27,7 @@ class HessianDiagonal(Base):
             path: Optional[str] = None,
             checkpoint: Optional[int] = None,
             device: torch.device = DEVICE,
-            loss: Literal["canonical", "meng"] = "canonical",
+            loss: Literal["lu", "meng"] = "meng",
             **hyperparameters
             ):
         if isinstance(score_model, ScoreModel):
@@ -32,35 +38,76 @@ class HessianDiagonal(Base):
             if not isinstance(score_model, ScoreModel):
                 raise ValueError("Must provide a ScoreModel instance to instantiate the HessianDiagonal model.")
             self.score_model = score_model
-        if loss == "canonical":
+        if loss.lower() == "lu":
             self._loss = second_order_dsm
-        elif loss == "meng":
+        elif loss.lower() == "meng":
             self._loss = second_order_dsm_meng_variation
         else:
-            raise ValueError(f"Loss function {loss} is not recognized. Choose 'canonical' or 'meng'.")
-        
+            raise ValueError(f"Loss function {loss} is not recognized. Choose between 'Lu' or 'Meng' loss functions.")
         # Make sure ScoreModel weights are frozen (this class does not allow joint optimization for now)
         for p in self.score_model.net.parameters():
             p.requires_grad = False
         print("Score model weights are now frozen. This class does not currently support joint optimization.")
    
-    def forward(self, t: Tensor, x: Tensor, *args):
-        return self.diagonal(t, x, *args)
+    def forward(self, t: Tensor, x: Tensor, *args, **kwargs) -> Tensor:
+        return self.diagonal(t, x, *args, **kwargs)
+    
+    def score(self, t: Tensor, x: Tensor, *args, **kwargs) -> Tensor:
+        return self.score_model.score(t, x, *args, **kwargs)
     
     def loss(self, x: Tensor, *args, step: int) -> Tensor:
         return self._loss(self, x, *args)
     
-    def reparametrized_diagonal(self, t: Tensor, x: Tensor, *args):
-        return self.net(t, x, *args)
+    def reparametrized_diagonal(self, t: Tensor, x: Tensor, *args, **kwargs) -> Tensor:
+        """
+        Numerically stable reparametrization of the diagonal of the Hessian for the DSM loss.
+        """
+        return self.net(t, x, *args, **kwargs)
     
-    def diagonal(self, t: Tensor, x: Tensor, *args):
+    def diagonal(self, t: Tensor, x: Tensor, *args, **kwargs) -> Tensor:
+        """
+        Diagonal of the Hessian of the log likelihood with respect to the input x.
+        """
         B, *D = x.shape
         sigma_t = self.sde.sigma(t).view(B, *[1]*len(D))
-        return (self.net(t, x, *args) - 1) / sigma_t**2
+        return (self.net(t, x, *args, **kwargs) - 1) / sigma_t**2
     
-    def trace(self, t: Tensor, x: Tensor, *args):
-        return self.diagonal(t, x, *args).flatten(1).sum(1)
-                
+    def dlogp(self, t: Tensor, x: Tensor, *args, dt: Tensor, **kwargs):
+        """
+        Compute the divergence of the probability flow ODE drift function 
+        to update the log probability. 
+        """
+        g = self.sde.diffusion(t, x).squeeze()
+        f, vjp_func = vjp(lambda x: self.sde.drift(t, x), x)
+        div_f = vjp_func(torch.ones_like(f))[0].flatten(1).sum(1)         # divergence of the drift
+        trace_H = self.diagonal(t, x, *args, **kwargs).flatten(1).sum(1)  # trace of the Hessian (divergence of the score)
+        return (div_f - 0.5 * g**2 * trace_H) * dt.squeeze()
+
+    def log_prob(
+        self,
+        x,
+        *args,
+        steps,
+        t=0.0,
+        solver: Literal["euler_ode", "rk2_ode", "rk4_ode"] = "euler_ode",
+        **kwargs
+    ) -> Tensor:
+        """
+        Compute the log likelihood of point x using the probability flow ODE,
+        which makes use of the instantaneous change of variable formula
+        developed by Chen et al. 2018 (arxiv.org/abs/1806.07366).
+        See Song et al. 2020 (arxiv.org/abs/2011.13456) for usage with SDE formalism of SBM.
+        """
+        B, *D = x.shape
+        solver = ODESolver(self, solver=solver, **kwargs)
+        # Solve the probability flow ODE up in temperature to time t=1.
+        xT, dlogp = solver(
+            x, *args, steps=steps, forward=True, t_min=t, **kwargs, return_dlogp=True, dlogp=self.dlogp
+        )
+        # add boundary condition PDF probability
+        logp = self.sde.prior(D).log_prob(xT) + dlogp
+        return logp
+    
     def save(
             self, 
             path: Optional[str] = None, 

src/score_models/sbm/score_model.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, Literal
+from typing import Union, Optional, Literal, TYPE_CHECKING
 
 from torch import Tensor
 from torch.nn import Module
@@ -9,6 +9,8 @@ from ..sde import SDE
 from ..losses import dsm
 from ..solver import Solver, ODESolver
 from ..utils import DEVICE
+if TYPE_CHECKING:
+    from score_models import HessianDiagonal
 
 
 __all__ = ["ScoreModel"]
@@ -21,12 +23,15 @@ class ScoreModel(Base):
         sde: Optional[Union[str, SDE]] = None,
         path: Optional[str] = None,
         checkpoint: Optional[int] = None,
-        hessian_trace_model: Optional[Union[str, Module]] = None,
+        hessian_diagonal_model: Optional["HessianDiagonal"] = None,
         device=DEVICE,
         **hyperparameters
     ):
         super().__init__(net, sde, path, checkpoint=checkpoint, device=device, **hyperparameters)
-        self.hessian_trace_model = hessian_trace_model
+        if hessian_diagonal_model is not None:
+            self.dlogp = hessian_diagonal_model.dlogp
+        else:
+            self.dlogp = None
 
     def loss(self, x, *args, step: int) -> Tensor:
         return dsm(self, x, *args)
@@ -48,10 +53,10 @@ class ScoreModel(Base):
     def score(self, t, x, *args, **kwargs) -> Tensor:
         _, *D = x.shape
         sigma_t = self.sde.sigma(t).view(-1, *[1] * len(D))
-        epsilon_theta = self.reparametrized_score(t, x, *args, **kwargs)
-        return epsilon_theta / sigma_t
+        epsilon = self.reparametrized_score(t, x, *args, **kwargs)
+        return epsilon / sigma_t
 
-    def log_likelihood(
+    def log_prob(
         self,
         x,
         *args,
@@ -67,22 +72,19 @@ class ScoreModel(Base):
         See Song et al. 2020 (arxiv.org/abs/2011.13456) for usage with SDE formalism of SBM.
         """
         B, *D = x.shape
-
         solver = ODESolver(self, solver=solver, **kwargs)
         # Solve the probability flow ODE up in temperature to time t=1.
-        xT, dlog_p = solver(
-            x, *args, steps=steps, forward=True, t_min=t, **kwargs, get_delta_logp=True
+        xT, dlogp = solver(
+            x, *args, steps=steps, forward=True, t_min=t, **kwargs, return_dlogp=True, dlogp=self.dlogp
         )
-
         # add boundary condition PDF probability
-        log_p = self.sde.prior(D).log_prob(xT) + dlog_p
-
-        return log_p
+        logp = self.sde.prior(D).log_prob(xT) + dlogp
+        return logp
 
     @torch.no_grad()
     def sample(
         self,
-        shape: tuple,  # TODO grab dimensions from model hyperparams if available
+        shape: tuple,
         steps: int,
         *args,
         solver: Literal[
@@ -112,7 +114,6 @@ class ScoreModel(Base):
             denoise_last_step=denoise_last_step,
             **kwargs
         )
-
         return x0
 
     @torch.no_grad()
@@ -145,7 +146,6 @@ class ScoreModel(Base):
             denoise_last_step=denoise_last_step,
             **kwargs
         )
-
         return x0
 
     def tweedie(self, t: Tensor, x: Tensor, *args, **kwargs) -> Tensor:

src/score_models/solver/ode.py
@@ -15,13 +15,14 @@ class ODESolver(Solver):
         x: Tensor,
         steps: int,
         forward: bool,
-        *args: tuple,
+        *args,
         progress_bar: bool = True,
         trace: bool = False,
         kill_on_nan: bool = False,
         denoise_last_step: bool = False,
         time_steps: Optional[Tensor] = None,
-        get_delta_logp: bool = False,
+        dlogp: Optional[Callable] = None,
+        return_dlogp: bool = False,
         hook: Optional[Callable] = None,
         **kwargs,
     ):
@@ -50,85 +51,70 @@ class ODESolver(Solver):
             trace: Whether to return the full path or just the last point.
             kill_on_nan: Whether to raise an error if NaNs are encountered.
             denoise_last_step: Whether to project to the boundary at the last step.
+            dlogp: Optional function to compute the divergence of the drift function.
+            return_dlogp: Whether to return the log probability change.
             time_steps: Optional time steps to use for integration. Should be a 1D tensor containing the bin edges of the
                 time steps. For example, if one wanted 50 steps from 0 to 1, the time steps would be ``torch.linspace(0, 1, 51)``.
-            get_delta_logp: Whether to return the log probability of the input x (should be used with forward=True).
             hook: Optional hook function to call after each step. Will be called with the signature ``hook(t, x, sde, score, solver)``.
         """
         B, *D = x.shape
-
-        # Step
-        T, dT = self.time_steps(steps, B, D, time_steps=time_steps, forward=forward, **kwargs)
-
-        # log P(xt) if requested
-        dlogp = torch.zeros(B, device=x.device, dtype=x.dtype)
-        if self.score.hessian_trace_model is None:
-            ht = self.divergence_hutchinson_trick
+        if return_dlogp:
+            if dlogp:
+                self.dlogp = dlogp
+            else:
+                self.dlogp = self.divergence_hutchinson_trick
         else:
-            ht = (
-                lambda t, x, args, dt, **kwargs: self.score.hessian_trace_model(
-                    t, x, *args, **kwargs
-                )
-                * dt
-            )
-        dp = ht if get_delta_logp else lambda *args, **kwargs: 0.0
+            self.dlogp = lambda t, x, *args, dt, **kwargs: torch.zeros(B, device=x.device, dtype=x.dtype)
 
-        # Trace ODE path if requested
+        T, dT = self.time_steps(steps, B, D, time_steps=time_steps, forward=forward, **kwargs)
+        logp = torch.zeros(B, device=x.device, dtype=x.dtype)
+
         if trace:
             path = [x]
-
-        # Progress bar
         pbar = tqdm(tuple(zip(T, dT))) if progress_bar else zip(T, dT)
         for t, dt in pbar:
+            ######### Update #########
+            dx, dlogp = self.step(t, x, *args, dt=dt, dx=self.dx, dp=self.dlogp, **kwargs)
+            x = x + dx
+            logp = logp + dlogp
+
+            ######### Logging #########
             if progress_bar:
                 pbar.set_description(
                     f"t={t[0].item():.1g} | sigma={self.sde.sigma(t)[0].item():.1g} | "
                     f"x={x.mean().item():.1g}\u00B1{x.std().item():.1g}"
                 )
-
-            # Check for NaNs
             if kill_on_nan and torch.any(torch.isnan(x)):
                 raise ValueError("NaN encountered in ODE solver")
-
-            # Update x
-            step = self.step(t, x, args, dt, self.dx, dp, **kwargs)
-            x = x + step[0]
-            dlogp = dlogp + step[1]
-
             if trace:
                 path.append(x)
-
-            # Call hook
             if hook is not None:
-                hook(t, x, self.sde, self.score, self)
+                hook(t, x, self.sde, self.sbm.score, self)
 
-        # Project to boundary if denoising
         if denoise_last_step and not forward:
             x = self.tweedie(t, x, *args, **kwargs)
             if trace:
                 path[-1] = x
-
-        # Return path or final x
         if trace:
-            if get_delta_logp:
-                return torch.stack(path), dlogp
+            if return_dlogp:
+                return torch.stack(path), logp
             return torch.stack(path)
-        if get_delta_logp:
-            return x, dlogp
+        if return_dlogp:
+            return x, logp
         return x
 
-    def dx(self, t: Tensor, x: Tensor, args: tuple, dt: Tensor, **kwargs):
+    def dx(self, t: Tensor, x: Tensor, *args, dt: Tensor, **kwargs):
         """Discretization of the ODE, this is the update for x"""
         f = self.sde.drift(t, x)
         g = self.sde.diffusion(t, x)
-        s = self.score(t, x, *args, **kwargs)
+        s = self.sbm.score(t, x, *args, **kwargs)
         return (f - 0.5 * g**2 * s) * dt
 
     def divergence_hutchinson_trick(
         self,
         t: Tensor,
         x: Tensor,
-        args: tuple,
+        *args,
         dt: Tensor,
         n_cot_vec: int = 1,
         noise_type: Literal["rademacher", "gaussian"] = "rademacher",
@@ -147,12 +133,13 @@ class ODESolver(Solver):
         """
         _, *D = x.shape
         # duplicate samples for for the Hutchinson trace estimator
-        samples = torch.tile(x, [n_cot_vec, *[1] * len(D)])
+        samples = torch.tile(x, [n_cot_vec, *[1]*len(D)])
         t = torch.tile(t, [n_cot_vec])
+        dt = torch.tile(dt, [n_cot_vec, *[1]*len(D)])
         _args = []
         for arg in args:
             _, *DA = arg.shape
-            arg = torch.tile(arg, [n_cot_vec, *[1] * len(DA)])
+            arg = torch.tile(arg, [n_cot_vec, *[1]*len(DA)])
             _args.append(arg)
 
         # sample cotangent vectors
@@ -160,9 +147,10 @@ class ODESolver(Solver):
         if noise_type == "rademacher":
             vectors = vectors.sign()
 
-        f = lambda x: self.dx(t, x, _args, dt, **kwargs)
+        f = lambda x: self.dx(t, x, *_args, dt=dt, **kwargs)
         _, vjp_func = vjp(f, samples)
         divergence = (vectors * vjp_func(vectors)[0]).flatten(1).sum(dim=1)
+        divergence = divergence.view(n_cot_vec, -1).mean(dim=0)
         return divergence
 
 
@@ -171,8 +159,8 @@ class Euler_ODE(ODESolver):
     Euler method for solving an ODE
     """
 
-    def step(self, t, x, args, dt, dx, dp, **kwargs):
-        return dx(t, x, args, dt, **kwargs), dp(t, x, args, dt, **kwargs)
+    def step(self, t, x, *args, dt, dx, dp, **kwargs):
+        return dx(t, x, *args, dt=dt, **kwargs), dp(t, x, *args, dt=dt, **kwargs)
 
 
 class RK2_ODE(ODESolver):
@@ -180,11 +168,11 @@ class RK2_ODE(ODESolver):
     Runge Kutta 2nd order ODE solver
     """
 
-    def step(self, t, x, args, dt, dx, dp, **kwargs):
-        k1 = dx(t, x, args, dt, **kwargs)
-        l1 = dp(t, x, args, dt, **kwargs)
-        k2 = dx(t + dt.squeeze(), x + k1, args, dt, **kwargs)
-        l2 = dp(t + dt.squeeze(), x + k1, args, dt, **kwargs)
+    def step(self, t, x, *args, dt, dx, dp, **kwargs):
+        k1 = dx(t, x, *args, dt=dt, **kwargs)
+        l1 = dp(t, x, *args, dt=dt, **kwargs)
+        k2 = dx(t + dt.squeeze(), x + k1, *args, dt=dt, **kwargs)
+        l2 = dp(t + dt.squeeze(), x + k1, *args, dt=dt, **kwargs)
         return (k1 + k2) / 2, (l1 + l2) / 2
 
 
@@ -193,13 +181,13 @@ class RK4_ODE(ODESolver):
     Runge Kutta 4th order ODE solver
     """
 
-    def step(self, t, x, args, dt, dx, dp, **kwargs):
-        k1 = dx(t, x, args, dt, **kwargs)
-        l1 = dp(t, x, args, dt, **kwargs)
-        k2 = dx(t + dt.squeeze() / 2, x + k1 / 2, args, dt, **kwargs)
-        l2 = dp(t + dt.squeeze() / 2, x + k1 / 2, args, dt, **kwargs)
-        k3 = dx(t + dt.squeeze() / 2, x + k2 / 2, args, dt, **kwargs)
-        l3 = dp(t + dt.squeeze() / 2, x + k2 / 2, args, dt, **kwargs)
-        k4 = dx(t + dt.squeeze(), x + k3, args, dt, **kwargs)
-        l4 = dp(t + dt.squeeze(), x + k3, args, dt, **kwargs)
+    def step(self, t, x, *args, dt, dx, dp, **kwargs):
+        k1 = dx(t, x, *args, dt=dt, **kwargs)
+        l1 = dp(t, x, *args, dt=dt, **kwargs)
+        k2 = dx(t + dt.squeeze() / 2, x + k1 / 2, *args, dt=dt, **kwargs)
+        l2 = dp(t + dt.squeeze() / 2, x + k1 / 2, *args, dt=dt, **kwargs)
+        k3 = dx(t + dt.squeeze() / 2, x + k2 / 2, *args, dt=dt, **kwargs)
+        l3 = dp(t + dt.squeeze() / 2, x + k2 / 2, *args, dt=dt, **kwargs)
+        k4 = dx(t + dt.squeeze(), x + k3, *args, dt=dt, **kwargs)
+        l4 = dp(t + dt.squeeze(), x + k3, *args, dt=dt, **kwargs)
         return (k1 + 2 * k2 + 2 * k3 + k4) / 6, (l1 + 2 * l2 + 2 * l3 + l4) / 6

src/score_models/solver/sde.py
@@ -93,7 +93,7 @@ class SDESolver(Solver):
 
             # Call hook
             if hook is not None:
-                hook(t, x, self.sde, self.score, self)
+                hook(t, x, self.sde, self.sbm.score, self)
 
         # Project to boundary if denoising
         if denoise_last_step and not forward:
@@ -110,7 +110,7 @@ class SDESolver(Solver):
         _, *D = x.shape
         z = torch.randn_like(x)
         epsilon = (snr * self.sde.sigma(t).view(-1, *[1] * len(D))) ** 2
-        return x + epsilon * self.score(t, x, *args, **kwargs) + z * torch.sqrt(2 * epsilon)
+        return x + epsilon * self.sbm.score(t, x, *args, **kwargs) + z * torch.sqrt(2 * epsilon)
 
     def drift(self, t: Tensor, x: Tensor, args: tuple, forward: bool, **kwargs):
         """SDE drift term"""
@@ -118,7 +118,7 @@ class SDESolver(Solver):
         if forward:
             return f
         g = self.sde.diffusion(t, x)
-        s = self.score(t, x, *args, **kwargs)
+        s = self.sbm.score(t, x, *args, **kwargs)
         return f - g**2 * s
 
     def dx(self, t, x, args, dt, forward, dw=None, **kwargs):

tests/test_architectures.py
@@ -19,12 +19,10 @@ def test_unets(D, C, ch_mult, nf, num_res_blocks, attention, Net):
     assert out.shape == torch.Size([B, C, *[P]*D])
     assert torch.isfinite(out).all()
 
+@pytest.mark.parametrize("width", [10, 50])
 @pytest.mark.parametrize("layers", [1, 3])
-@pytest.mark.parametrize("time_branch_channels", [16, 32])
-@pytest.mark.parametrize("time_branch_layers", [1, 3])
-@pytest.mark.parametrize("bottleneck", [10, 20])
 @pytest.mark.parametrize("attention", [True, False])
-def test_mlp(layers, time_branch_channels, time_branch_layers, bottleneck, attention):
+def test_mlp(width, layers, attention):
     B = 2
     C = 10
     x = torch.randn(B, C)
@@ -32,9 +30,7 @@ def test_mlp(layers, time_branch_channels, time_branch_layers, bottleneck, atten
     model = MLP(
             C, 
             layers=layers, 
-            time_branch_channels=time_branch_channels, 
-            time_branch_layers=time_branch_layers, 
-            bottleneck=bottleneck, 
+            width=width,
             attention=attention)
     out = model(t, x)
     assert out.shape == torch.Size([B, C])